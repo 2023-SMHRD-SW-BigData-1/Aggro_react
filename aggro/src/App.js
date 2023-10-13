fbb5cc51019e36bd7a426a4c6bfebdceebe106e3
오승원 <netakasa@gmail.com>
2023-10-13 15:46:19 +0900

    2023-10-13 15:46 오승원 cors 재설정 및 회원가입 기본틀 작성

--- aggro/src/App.js
+++ aggro/src/App.js
@@ -1,22 +1,61 @@
 import logo from './logo.svg';
 import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
 
   const axiosTest = () => {
-      axios.get('/bigdata/test')
-      .then((res)=>{
+    axios.get('bigdata/test')
+      .then((res) => {
         console.log(res)
       })
-      .catch(()=>{})
+      .catch(() => { })
   }
+
+  const idRef = useRef();
+  const pwRef = useRef();
+  const nickRef = useRef();
+
+  const [userData, setUserData] = useState({})
+
+  const handleAxios = (e) => {
+    e.preventDefault();
+
+    setUserData({
+      user_id: idRef.current.value,
+      user_pw: pwRef.current.value,
+      user_nick: nickRef.current.value,
+      user_class: "0",
+    })
+  }
+
+  useEffect(() => {
+    userData.user_id !== undefined && 
+      axios.post("bigdata/form", userData)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch(() => {
+
+        })
+  }, [userData])
 
   return (
     <div className="">
       <button onClick={axiosTest}>
         통신 테스트 버튼
       </button>
+
+      <form onSubmit={handleAxios}>
+        <input type='text' placeholder='Id' ref={idRef} />
+        <br />
+        <input type='password' placeholder='Pw' ref={pwRef} />
+        <br />
+        <input type='text' placeholder='Nick' ref={nickRef} />
+        <br />
+        <input type='submit' value="form 타입 테스트" />
+      </form>
     </div>
   );
 }
