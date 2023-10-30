18c6c2330f01885dfc21c16bf1c874bb14c4d332
정준옥 <wnsdhr7221@gmail.com>
2023-10-30 10:32:13 +0900

    231030 준옥 오전

--- aggro/src/pages/ranking/Ranking.js
+++ aggro/src/pages/ranking/Ranking.js
@@ -12,66 +12,84 @@ import RankingBar from "./RankingBar";
 import WordCloud from "./WordCloud";
 import AgreeViewTab from "./AgreeViewTab";
 
-const Ranking = ({ match }) => {
+const Ranking = ({ history }) => {
   const [data, setData] = useState([
     { x: "중립", y: 50, color: "#cfd9df" },
     { x: "부정", y: 40, color: "#c2e9fb" },
     { x: "긍정", y: 10, color: '#a1c4fd' }
   ]);
 
-  const pdfRef = useRef(); // This ref is for capturing the App div for pdf generation
+  const [username, setUsername] = useState("");
+  
+  const handleInput = (e) => {
+    setUsername(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push("/ranking/" + username);
+  };
+
+  const pdfRef = useRef(); 
 
   const downloadPDF = () => {
     const input = pdfRef.current; 
 
     html2canvas(input, {
       scale: 1,  // 해상도 향상
-        useCORS: true, // CORS 문제 해결을 위해
+      useCORS: true, // CORS 문제 해결
     }).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('landscape', 'mm', 'a4', true);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const imgWidth = canvas.width;
-        const imgHeight = canvas.height;
-        const heightRatio = pdfWidth * (imgHeight / imgWidth);
-        
-        // 페이지 마진 조정
-        const margin = 10; // 10mm 마진
-        pdf.addImage(imgData, 'PNG', margin, margin, pdfWidth - 2 * margin, heightRatio - 2 * margin);
-        
-        pdf.save('Report.pdf');
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('landscape', 'mm', 'a4', true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const heightRatio = pdfWidth * (imgHeight / imgWidth);
+      const margin = 10;
+      pdf.addImage(imgData, 'PNG', margin, margin, pdfWidth - 2 * margin, heightRatio - 2 * margin);
+      pdf.save('Report.pdf');
     });
-};
-
-  let setStateInterval;
+  };
 
   useEffect(() => {
-    setStateInterval = setInterval(() => {
-      let a = Math.random(); 
-      let b = Math.random(); 
-      let c = Math.random(); 
+    const interval = setInterval(() => {
+      let a = Math.random();
+      let b = Math.random();
+      let c = Math.random();
 
-      let data_legacy = [
+      const newData = [
         { x: "중립", y: a / (a + b + c) * 100, color: "#cfd9df" },
         { x: "부정", y: b / (a + b + c) * 100, color: "#c2e9fb" },
         { x: "긍정", y: c / (a + b + c) * 100, color: '#a1c4fd' }
-      ].slice().sort((a, b) => a.y - b.y);
+      ].sort((a, b) => a.y - b.y);
 
-      setData(data_legacy);
+      setData(newData);
     }, 2000);
 
-    return () => {
-      clearInterval(setStateInterval);
-    };
-  }, [data]);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
       <Header1 />
-      <div className="btn-box">
+      
+        <div className="btn-box">
+      <form onSubmit={handleSubmit} className="summoner-search-form">
+        <input
+          type="text"
+          name="username"
+          onChange={handleInput}
+          className="summoner-search-form__text__suggest"
+          placeholder="검색어를 입력해주세요."
+        />
+        <button type="submit" className="summoner-search-form__button">
+          <img src="Aggro_blue_mini.png" alt="검색" className="btnImg" />
+        </button>
+      </form>
+          <div className="searchbar"></div>
         <button className="PDF-btn" onClick={downloadPDF}>다운로드 PDF</button>
       </div>
-
       <div className="App" ref={pdfRef}>
         <div className="grid-container">
           <div className="grid-item">
@@ -84,8 +102,7 @@ const Ranking = ({ match }) => {
           </div>
           <div className="grid-item">
               <p className="item-box-item">워드클라우드</p>
-              <RankingBar className="item-box-item" />
-               {/*<WordCloud className="item-box-item" />  워드클라우드 컴포넌트로 수정 */}
+              <WordCloud className="item-box-item" />
           </div>
           <div className="grid-item">
               <p className="item-box-item">긍부정</p>
@@ -93,10 +110,8 @@ const Ranking = ({ match }) => {
           </div>
           <div className="grid-item merged">
               <p className="item-box-item merged">뷰탭</p>
-              <AgreeViewTab className="item-box-item merged" /> {/* 동의보기 컴포넌트로 수정 */}
+              <AgreeViewTab className="item-box-item merged" />
           </div>
-
-
         </div>
       </div>
       <Footer2 />
