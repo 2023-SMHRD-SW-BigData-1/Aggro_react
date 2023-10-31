f4e6540b880d0e65883a13b0a277f09cf76d158f
오승원 <netakasa@gmail.com>
2023-10-31 16:45:36 +0900

    2023-10-31 16:45 오승원. 뷰탭 완료

--- aggro/src/pages/ranking/Ranking.js
+++ aggro/src/pages/ranking/Ranking.js
@@ -13,13 +13,9 @@ import AgreeViewTab from "./AgreeViewTab";
 import imgSearch from "./Aggro_blue_mini.png";
 import MapWord from "./MapWord";
 import BarChartComponent from './BarChartComponent';
+import Axios from "axios";
 
 const Ranking = ({ history, match }) => {
-  const [data, setData] = useState([
-    { x: "중립", y: 50, color: "#cfd9df" },
-    { x: "부정", y: 40, color: "#c2e9fb" },
-    { x: "긍정", y: 10, color: '#a1c4fd' }
-  ]);
 
   const [username, setUsername] = useState("");
 
@@ -38,7 +34,7 @@ const Ranking = ({ history, match }) => {
   const downloadPDF = () => {
     // PDF 생성을 위한 참조 (DOM 요소)
     const input = pdfRef.current;
-  
+
     // html2canvas 라이브러리를 사용하여 HTML 요소를 캔버스로 변환
     html2canvas(input, {
       scale: 1, // 스케일 설정
@@ -49,30 +45,39 @@ const Ranking = ({ history, match }) => {
     }).then((canvas) => {
       // 캔버스를 이미지 데이터로 변환
       const imgData = canvas.toDataURL('image/png');
-  
+
       // jsPDF 인스턴스 생성 (가로 방향, 단위는 'mm', A4 크기, 높은 해상도로 설정)
       const pdf = new jsPDF('landscape', 'mm', 'a4', true);
-  
+
       // PDF 페이지의 너비 계산
       const pdfWidth = pdf.internal.pageSize.getWidth();
-  
+
       // 캔버스의 너비와 높이 계산
       const imgWidth = canvas.width;
       const imgHeight = canvas.height;
-  
+
       // 이미지의 높이와 너비 비율을 계산
       const heightRatio = pdfWidth * (imgHeight / imgWidth);
-  
+
       // PDF 내 이미지의 위치와 크기를 설정하기 위한 여백 값
       const margin = 30;
-  
+
       // 이미지를 PDF에 추가
       pdf.addImage(imgData, 'PNG', margin, margin, pdfWidth - 2 * margin, heightRatio - 2 * margin);
-  
+
       // 'Report.pdf' 이름으로 PDF 저장
       pdf.save('Report.pdf');
     });
   };
+
+  // 긍부정 데이터
+
+  const [pieData, setPieData] = useState([
+    { x: "중립", y: 50, color: "#cfd9df" },
+    { x: "부정", y: 40, color: "#c2e9fb" },
+    { x: "긍정", y: 10, color: '#a1c4fd' }
+  ]);
+
   useEffect(() => {
     const interval = setInterval(() => {
       let a = Math.random();
@@ -85,11 +90,34 @@ const Ranking = ({ history, match }) => {
         { x: "긍정", y: c / (a + b + c) * 100, color: '#a1c4fd' }
       ].sort((a, b) => a.y - b.y);
 
-      setData(newData);
+      setPieData(newData);
     }, 2000);
 
     return () => clearInterval(interval);
-  }, [data]);
+  }, [pieData]);
+  // 긍부정 데이터 가공 끝
+
+  // 서치데이터 처리
+
+  const searchName = match.params.searchName
+
+  const [searchData, setSearchData] = useState([]) // 특정 키워드로 가져온 데이터
+
+  useEffect(() => {
+    if (searchName) {
+      Axios
+        .get("http://localhost:8283/bigdata/ranking/detail/" + searchName)
+        .then((response) => {
+
+          setSearchData(response.data)
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
+  }, [searchName])
+  // 서치데이터 가공 처리 끝
+
 
   return (
     <>
@@ -128,10 +156,10 @@ const Ranking = ({ history, match }) => {
             <MapWord className="item-box-item mapword-styled" />
           </div>
           <div className="grid-item">
-          <p className="item-box-item">가제 2</p>
-          <RankingBar className="item-box-item" /> {/* 이 부분을 수정했습니다. */}
-        </div>
-        <div className="grid-item">
+            <p className="item-box-item">가제 2</p>
+            <RankingBar className="item-box-item" /> {/* 이 부분을 수정했습니다. */}
+          </div>
+          <div className="grid-item">
             <p className="item-box-item">키워드별 검색량</p>
             <RankingBar className="item-box-item" />
           </div>
@@ -139,7 +167,7 @@ const Ranking = ({ history, match }) => {
 
           <div className="grid-item merged">
             <p className="item-box-item merged">월간 검색량</p>
-            <SearchData className="item-box-item merged" searchName={match.params.searchName} />
+            <SearchData className="item-box-item merged" searchData={searchData} />
           </div>
 
 
@@ -149,11 +177,11 @@ const Ranking = ({ history, match }) => {
           </div>
           <div className="grid-item">
             <p className="item-box-item">긍부정</p>
-            <CircularProgressBar className="item-box-item" data={data} />
+            <CircularProgressBar className="item-box-item" data={pieData} />
           </div>
           <div className="grid-item merged">
             <p className="item-box-item merged">뷰탭</p>
-            <AgreeViewTab className="item-box-item merged" />
+            <AgreeViewTab className="item-box-item merged" searchData={searchData} />
           </div>
         </div>
       </div>
--- aggro/src/pages/ranking/SearchData.jsx
+++ aggro/src/pages/ranking/SearchData.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { VictoryAxis, VictoryBar, VictoryBrushContainer, VictoryChart, VictoryLine, VictoryTooltip, VictoryZoomContainer } from 'victory'
+import { VictoryAxis, VictoryBar, VictoryBrushContainer, VictoryChart, VictoryLine, VictoryScatter, VictoryTooltip, VictoryZoomContainer } from 'victory'
 import SearchLabel from './SearchLabel'
 
 
-const SearchData = ({ searchName }) => {
+const SearchData = ({ searchData }) => {
     const [state, setState] = useState({
         zoomDomain: {
             x: [
@@ -22,39 +22,42 @@ const SearchData = ({ searchName }) => {
 
 
     useEffect(() => {
-        if (searchName) {
-            axios
-                .get("http://localhost:8283/bigdata/ranking/detail/" + searchName)
-                .then((response) => {
-                    // 응답 데이터를 처리
-                    const processedData = [];
-                    const dataMap = new Map();
+        console.log(searchData && searchData.length > 0);
 
-                    response.data.forEach((item) => {
-                        const crawlAt = new Date(item.crawlAt).toISOString().slice(0, 7); // YYYY-MM-DD 형식으로 날짜 포맷 변환
+        if (searchData.length > 0) {
+            // 응답 데이터를 처리
+            const processedData = [];
+            const dataMap = new Map();
 
-                        if (dataMap.has(crawlAt)) {
-                            dataMap.set(crawlAt, dataMap.get(crawlAt) + 1); // 날짜가 이미 있으면 b 값을 증가
-                        } else {
-                            dataMap.set(crawlAt, 1); // 날짜가 없으면 새로운 날짜로 초기화
-                        }
-                    });
+            searchData.forEach((item) => {
+                const crawlAt = new Date(item.crawlAt).toISOString().slice(0, 7); // YYYY-MM-DD 형식으로 날짜 포맷 변환
 
-                    dataMap.forEach((value, key) => {
-                        processedData.push({
-                            a: new Date(key),
-                            b: value,
-                        });
-                    });
+                if (dataMap.has(crawlAt)) {
+                    dataMap.set(crawlAt, dataMap.get(crawlAt) + 1); // 날짜가 이미 있으면 b 값을 증가
+                } else {
+                    dataMap.set(crawlAt, 1); // 날짜가 없으면 새로운 날짜로 초기화
+                }
+            });
 
-                    // 데이터를 상태로 설정
-                    setData(processedData);
-                })
-                .catch((error) => {
-                    console.log(error);
+            dataMap.forEach((value, key) => {
+                processedData.push({
+                    a: new Date(key),
+                    b: value,
                 });
-        } else {
+            });
 
+            // 데이터를 상태로 설정
+            setData(processedData);
+            setState({
+                zoomDomain: {
+                    x: [
+                        new Date().setFullYear(new Date().getFullYear() - 3),
+                        new Date()
+                    ]
+                }
+            })
+
+        } else {
             setData([
                 { a: new Date(1982, 1, 1), b: 125 },
                 { a: new Date(1987, 1, 1), b: 257 },
@@ -66,7 +69,7 @@ const SearchData = ({ searchName }) => {
                 { a: new Date(2015, 1, 1), b: 470 }
             ])
         }
-    }, [searchName])
+    }, [searchData])
 
     return (
 
@@ -74,6 +77,14 @@ const SearchData = ({ searchName }) => {
 
             <VictoryChart
                 padding={{ top: 0, bottom: 40, left: 50, right: 50 }}
+                containerComponent={
+                    <VictoryZoomContainer
+                        zoomDomain={state.zoomDomain}
+                        onZoomDomainChange={handleZoom.bind(this)}
+                        allowZoom={false}
+
+                    />
+                }
             >
 
                 <VictoryLine
@@ -84,6 +95,22 @@ const SearchData = ({ searchName }) => {
                     data={data}
                     x="a"
                     y="b"
+
+                />
+                <VictoryScatter
+                    style={{
+                        data: { fill: "tomato" },
+
+                    }}
+                    data={data}
+                    x="a"
+                    y="b"
+                    labels={({ datum }) => `일시: ${datum.a.toLocaleDateString()}\n검색량:${datum.b}`}
+                    labelComponent={
+                        <VictoryTooltip
+                            constrainToVisibleArea
+                        />
+                    }
 
                 />
             </VictoryChart>
