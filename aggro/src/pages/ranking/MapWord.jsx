0a9729f96934e84414e95b80c7c24626844d44eb
정준옥 <wnsdhr7221@gmail.com>
2023-11-01 14:27:15 +0900

    231101 준옥

--- aggro/src/pages/ranking/MapWord.jsx
+++ aggro/src/pages/ranking/MapWord.jsx
@@ -3,7 +3,7 @@ import ReactWordcloud from 'react-wordcloud';
 import "./Ranking.css";
 
 const words = [
-  { text: 'React', value: 1 },
+  { text: 'React', value: 1, colors: "blue"},
   { text: 'JavaScript', value: 1 },
   { text: 'Node', value: 1 },
   { text: 'Express', value: 1 },
@@ -41,22 +41,23 @@ function MapWord() {
   const [hoveredWord, setHoveredWord] = useState(null);
 
   const options = {
-    enableTooltip: false,
-    deterministic: true,
-    rotations: 1,
-    rotationAngles: [0],
-    transitionDuration: 1,
-    fontFamily: 'sans-serif',
-    fontWeight: '700',
-    padding: 3,
-    // 각 단어의 색상을 동적으로 결정
-    colors: (word) => word.text === hoveredWord ? 'black' : 'white',
+    enableTooltip: false,       // 툴팁 활성화 여부. 현재는 비활성화 상태
+    deterministic: true,       // 레이아웃의 결정론적 생성 여부. true로 설정하면 항상 동일한 레이아웃을 생성
+    rotations: 0,              // 회전 횟수. 
+    rotationAngles: [0],       // 회전 각도. 현재는 0도만 사용
+    transitionDuration: 2000,     // 전환 지속 시간. 1로 설정하여 전환 효과의 지속 시간을 짧게 설정
+    fontFamily: 'sans-serif',  // 사용할 글꼴
+    fontWeight: '700',         // 글꼴의 굵기. 
+    padding: 0,                // 단어 간의 여백
+    // 각 단어의 색상을 동적으로 결정. 현재 마우스 오버된 단어는 'black', 그렇지 않으면 'white'로 설정
+    colors: (word) => word.text === hoveredWord ? '#000000' : '#FFFFFF',
   };
 
   // 단어 위에 마우스가 올라갔을 때와 나갈 때의 콜백 함수
   const callbacks = {
     onWordMouseOver: useCallback((event) => setHoveredWord(event.text), []),
     onWordMouseOut: useCallback(() => setHoveredWord(null), []),
+    
   };
 
   return (
--- aggro/src/pages/ranking/Ranking.js
+++ aggro/src/pages/ranking/Ranking.js
@@ -21,6 +21,8 @@ const Ranking = ({ history, match }) => {
     { x: "긍정", y: 10, color: '#a1c4fd' }
   ]);
 
+  const searchName = match.params.userName
+
   const [username, setUsername] = useState("");
 
   const handleInput = (e) => {
@@ -36,40 +38,22 @@ const Ranking = ({ history, match }) => {
   const pdfRef = useRef();
 
   const downloadPDF = () => {
-    // PDF 생성을 위한 참조 (DOM 요소)
     const input = pdfRef.current;
-  
-    // html2canvas 라이브러리를 사용하여 HTML 요소를 캔버스로 변환
     html2canvas(input, {
-      scale: 1, // 스케일 설정
-      useCORS: true, // CORS 문제를 해결하기 위한 설정
-      scrollY: -window.scrollY, // 현재 스크롤 위치를 고려하여 캡처
-      windowWidth: input.clientWidth, // 캡처할 요소의 너비 사용
-      windowHeight: input.clientHeight // 캡처할 요소의 높이 사용
+      scale: 1,
+      useCORS: true,
+      scrollY: -window.scrollY, // 스크롤 문제를 해결하기 위해 추가
+      windowWidth: input.clientWidth,  // 클라이언트 너비를 사용
+      windowHeight: input.clientHeight  // 클라이언트 높이를 사용
     }).then((canvas) => {
-      // 캔버스를 이미지 데이터로 변환
       const imgData = canvas.toDataURL('image/png');
-  
-      // jsPDF 인스턴스 생성 (가로 방향, 단위는 'mm', A4 크기, 높은 해상도로 설정)
       const pdf = new jsPDF('landscape', 'mm', 'a4', true);
-  
-      // PDF 페이지의 너비 계산
       const pdfWidth = pdf.internal.pageSize.getWidth();
-  
-      // 캔버스의 너비와 높이 계산
       const imgWidth = canvas.width;
       const imgHeight = canvas.height;
-  
-      // 이미지의 높이와 너비 비율을 계산
       const heightRatio = pdfWidth * (imgHeight / imgWidth);
-  
-      // PDF 내 이미지의 위치와 크기를 설정하기 위한 여백 값
       const margin = 30;
-  
-      // 이미지를 PDF에 추가
       pdf.addImage(imgData, 'PNG', margin, margin, pdfWidth - 2 * margin, heightRatio - 2 * margin);
-  
-      // 'Report.pdf' 이름으로 PDF 저장
       pdf.save('Report.pdf');
     });
   };
@@ -89,7 +73,7 @@ const Ranking = ({ history, match }) => {
     }, 2000);
 
     return () => clearInterval(interval);
-  }, [data]);
+  }, []);
 
   return (
     <>
@@ -123,34 +107,28 @@ const Ranking = ({ history, match }) => {
 
       <div className="App" ref={pdfRef}>
         <div className="grid-container">
-          <div className="grid-item">
-            <p className="item-box-item">map 1</p>
-            <MapWord className="item-box-item mapword-styled" />
-          </div>
-          <div className="grid-item">
-          <p className="item-box-item">가제 2</p>
-          <RankingBar className="item-box-item" /> {/* 이 부분을 수정했습니다. */}
-        </div>
-        <div className="grid-item">
-            <p className="item-box-item">키워드별 검색량</p>
-            <RankingBar className="item-box-item" />
-          </div>
-
-
           <div className="grid-item merged">
             <p className="item-box-item merged">월간 검색량</p>
-            <SearchData className="item-box-item merged" searchName={match.params.searchName} />
-          </div>
-
-
-          <div className="grid-item">
-            <p className="item-box-item">워드클라우드</p>
-            <WordCloud className="item-box-item" />
+            <SearchData className="item-box-item merged" searchName = {searchName}/>
           </div>
           <div className="grid-item">
             <p className="item-box-item">긍부정</p>
             <CircularProgressBar className="item-box-item" data={data} />
           </div>
+          <div className="grid-item">
+            <p className="item-box-item">워드클라우드</p>
+            <MapWord className="item-box-item mapword-styled" />
+          </div>
+          <div className="grid-item">
+          <p className="item-box-item">가제 2</p>
+          <RankingBar className="item-box-item" /> 
+        </div>
+          <div className="grid-item">
+            <p className="item-box-item">키워드별 검색량</p>
+            <RankingBar className="item-box-item" />
+          </div>
+
+
           <div className="grid-item merged">
             <p className="item-box-item merged">뷰탭</p>
             <AgreeViewTab className="item-box-item merged" />
