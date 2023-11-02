2848490dff07d2fb459b9f93309907e895cf609f
정준옥 <wnsdhr7221@gmail.com>
2023-11-02 15:20:13 +0900

    고고

--- aggro/src/pages/info/Intro.js
+++ aggro/src/pages/info/Intro.js
@@ -4,20 +4,44 @@ import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import Footer2 from "../../include/Footer2";
 import Header1 from "../../include/Header1";
 
+import img1 from "./1.jpg"
+import img2 from "./2.jpg"
+import img4 from "./4.png"
+
+
+
 function Intro() {
   return (
-<>
-<Header1 />
-    <div className="intro-container">
-      <h1>test!</h1>
-      <p>
-       test1
-      </p>
-    </div>
-<Footer2 />
-</>
-  );
+    <>
+      <Header1 />
+      <div className="intro-container">
+        <h1 className='text merged fw'>세상의 모든 관심</h1>
+        <h1 className='text merged fw'>' Aggro '</h1>
+        <div>
+          <div className="pic"><img src={img1} alt="image1" /></div>
+        </div>
 
+        <div>
+          <div className='text'>text1</div>
+        </div>
+
+        <div>
+        <div className='text'>text2</div>
+        </div>
+
+        <div>
+          <div className="pic"><img src={img2} alt="image2" /></div>
+        </div>
+
+        <div className="pic"><img src={img4} alt="image4" /></div>
+
+        <div>
+        <div className='text'>text4</div>
+        </div>
+      </div>
+      <Footer2 />
+    </>
+  );
 }
 
 export default Intro;
